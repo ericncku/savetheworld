3ccba7b0131a98713ead737c51d8c9f67f1ce887
HOISIO LONG <hoisiolong@HOISIOdeMacBook-Air.local>
2017-02-10 14:58:00 +0800

    Initial Commit

---
+++ SaveTheWorld/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SaveTheWorld
+//
+//  Created by HOISIO LONG on 10/2/2017.
+//  Copyright © 2017年 Eric Hoi. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
